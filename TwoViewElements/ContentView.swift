81e0119ee6c23104348d5682939d57aa5a3257f7
pumpklh <127506490+pumpklh@users.noreply.github.com>
2024-01-24 17:08:20 +0600

    Initial Commit

---
+++ TwoViewElements/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoViewElements
+//
+//  Created by Adilbek Mels on 24.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwoViewElements/TwoViewElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoViewElementsApp.swift
+//  TwoViewElements
+//
+//  Created by Adilbek Mels on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwoViewElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
